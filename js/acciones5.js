f062f2f5e13f45965e69126107d62a27786041ba
Guzman2001 <yazminguzmanmerino2001@gmail.com>
2018-05-23 16:15:47 -0500

    4

--- js/acciones5.js
+++ js/acciones5.js
@@ -1,23 +1,11 @@
 // JavaScript Document
-
-$(document).ready(function(e) {
-    document.addEventListener("deviceready",function(){
-		$('#izquierda').on("swipeleft", function(){
-			navigator.notification.alert("Deslizó hacia la izquierda",function(){"aplicacion 7","Ok"});
-		}); //cerrar deslizar izquierda
-		$('#derecha').on("swiperight",function(){
-			navigator.notification.confirm("¿Qué quieres hacer?",function(op){
-				switch(op)
-				{ 
-				   case 1:
-				   navigator.notification.beep(1);
-				   break;
-				   
-				   case 2:
-				   navigator.notification.vibrate(3000);
-				   break;
-				}
-		},"aplicacion 7","Beepear,Vibrar,Cancelar");
-	}); //cerar deslizar derecha
-	},false); //cerrar diveready
-}); //cerrar document
+$(document).ready(function(e){
+	document.addEventListener("deviceready", function(){
+		$('#beep').tap(function(){
+			navigator.notification.beep(2);
+		}); //cierre del beep
+		$('#vibrar').tap(function(){
+			navigator.notification.vibrate(2000);
+		}); //cierre del vibrar
+	},false); //cierre del deviceready
+}); //cierre ready
